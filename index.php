b83a28d1030e023fd7c7fba39e976d554dde11ca
Bastien ROBERT <bastien.robert@sepefrei.fr>
2016-10-26 14:28:28 -0400

    Header

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <title>Bread Express</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/master.css">
+    <link rel="stylesheet" href="assets/lib/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <nav id="navbar">
+
+        <div class="row">
+            <div class="logo"></div>
+
+            <div class="user">
+                <div><i class="fa fa-user" aria-hidden="true"></i>Sign in</div>
+                <div><i class="fa fa-edit" aria-hidden="true"></i>Register</div>
+            </div>
+
+
+            <div class="cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Cart</div>
+        </div>
+
+    </nav>
+
+</body>
+
+</html>

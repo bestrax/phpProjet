cbb7267aa86c8d0796620444c78a7185297d0bc5
Bastien ROBERT <bastien.robert@sepefrei.fr>
2016-10-27 09:47:02 -0400

    Base

--- index.php
+++ index.php
@@ -13,16 +13,13 @@
 
         <div class="row menu">
             <div class="logo">
-                <!--<img class="hidden-lg"src="assets/img/logo240.png" />
-                <img class="hidden-sm"src="assets/img/logo.png" />-->
-                <img src="assets/img/logodemi.png" />
+                <img src="assets/img/logodemi.svg" />
             </div>
 
             <div class="user">
                 <div><i class="fa fa-user" aria-hidden="true"></i>Sign in</div>
                 <div><i class="fa fa-edit" aria-hidden="true"></i>Register</div>
             </div>
-
 
             <div class="cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Cart</div>
         </div>
@@ -34,12 +31,6 @@
                 <div class="step">Selection</div>
                 <div class="step">Validation</div>
             </div>
-
-           <!-- <ul>
-                <li>Where and When</li>
-                <li>Selection</li>
-                <li>Validation</li>
-            </ul>-->
 
         </div>
 

474fbffada2ed6c12d26aba0439cb242b13cf3d3
Armelle Riviere <Armelle Riviere>
2016-11-06 21:56:48 -0500

    ajout des produits / categories

--- product.php
+++ product.php
@@ -1,173 +1,103 @@
-<html>
+<?php
+	session_start();	
 
-<head>
+	require_once 'assets/php/config/bdd.php';
+	$req = $bdd->query('SELECT * FROM category ');
+	$categories = $req->fetchAll();
 
-	<title>Bread Express</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/master.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/product.css">
-    <link rel="stylesheet" href="assets/lib/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=ABeeZee" rel="stylesheet">
+	if(empty($_GET['category']))
+		$category = $categories[0]['id'];
+	else
+		$category = $_GET['category'];
 
-</head>
+	$req = $bdd->prepare('SELECT * FROM product WHERE category_id=:category_id');
+	$req->execute(array(':category_id' => $category));
+	$products = $req->fetchAll();
+?>
 
-<body>
+<?php
+    include 'assets/php/partials/header.php';
+    pageHeader('', ['product'], 2);
+?>
 
-	<nav id="navbar">
+	
+<div id="container">
 
-		<div class="row menu">
-			<div class="logo">
-				<a href="index.php"><img src="assets/img/logodemi.jpg" /></a>
-			</div>
+	<div class="content-categorie">
+		<?php
+			foreach($categories as $category) {
+				?>
+				<div class="categorie">	
+					<a class="no-link" href="product.php?category=<?php echo $category['id']; ?>" >
+				 		<?php echo $category['name'].' '; ?>
+				 	</a>
+				</div>  	
+				<?php
+			} 
+		?>
+	
+	</div>
 
-			<div class="user">
-				<div><a href="login.php" class="no-link"><i class="fa fa-user" aria-hidden="true"></i>Sign in</a></div>
-				<div><a href="register.php" class="no-link"><i class="fa fa-edit" aria-hidden="true"></i>Register</a></div>
-			</div>
+	<div class="row margin-top-row">
+		
+		<div class="full">
+			<?php
+				foreach($products as $product) { 
+					?>
+					<div class="product">
+						<a class="title"><?php echo $product['name']; ?></a>
+						<img src="croissant.jpg" alt="croissant" />
+						<a class="description"><?php echo $product['description']; ?></a>
+						<a class="prix"><?php echo $product['price']; ?></a>
 
-			<div class="cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Cart</div>
+						<div class="add_sup">
+							<div class="command">
+								<a class="product_minus">-</a>
+								<div class="product_number">1</div>
+								<a class="product_plus">+</a>
+							</div>
+
+							<div >
+								<a class="button">ADD</a>
+							</div>
+						</div>
+
+					</div>
+					<?php
+				}
+			?>
+
 		</div>
 
-		<div class="row" id="order-process">
-
-			<div class="process">
-				<div class="step">Where and When ?</div>
-				<div class="step active">Selection</div>
-				<div class="step">Validation</div>
+		<div class="right-cart">
+			<a class="top"><i class="fa fa-shopping-cart" aria-hidden="true">&nbsp &nbsp</i>cart</a>
+			<div class="button button_order"><a href="cart.php" class="no-link">Validate your order</a></div>
+			<div class="text_pickup">
+				Pick-up : 11AM
 			</div>
-
-		</div>
-
-
-	</nav>
-
-	<div id="container">
-
-		<div class="content-categorie">
-			<div class="categorie active">Breakfast</div>
-			<div class="categorie">Healthy Breakfast</div>
-			<div class="categorie">Gluten fre breakfast</div>
-		</div>
-
-		<div class="row margin-top-row">
-
-			<div class="full">
-				 <div class="product">
-					<a class="title">Croissant</a>
-					<img src="croissant.jpg" alt="croissant" />
-					<a class="description">Croissant saveur de France ...</a>
-					<a class="prix">1€</a>
-
-					<div class="add_sup">
-						<div class="command">
-							<a class="product_minus">-</a>
-							<div class="product_number">1</div>
-							<a class="product_plus">+</a>
-						</div>
-
-						<div >
-							<a class="button">ADD</a>
-						</div>
-					</div>
-
-				</div>
-
-				<div class="product">
-					<a class="title">Pain</a>
-					<img src="croissant.jpg" alt="croissant" />
-					<a class="description">Croissant saveur de France ...</a>
-					<a class="prix">1€</a>
-
-					<div class="add_sup">
-						<div class="command">
-							<a class="product_minus">-</a>
-							<div class="product_number">1</div>
-							<a class="product_plus">+</a>
-						</div>
-
-						<div >
-							<a class="button">ADD</a>
-						</div>
-					</div>
-
-				</div>
-
-				 <div class="product">
-					<a class="title">Croissant</a>
-					<img src="croissant.jpg" alt="croissant" />
-					<a class="description">Croissant saveur de France ...</a>
-					<a class="prix">1€</a>
-
-					<div class="add_sup">
-						<div class="command">
-							<a class="product_minus">-</a>
-							<div class="product_number">1</div>
-							<a class="product_plus">+</a>
-						</div>
-
-						<div >
-							<a class="button">ADD</a>
-						</div>
-					</div>
-
-				</div>
-
-				<div class="product">
-					<a class="title">Croissant</a>
-					<img src="croissant.jpg" alt="croissant" />
-					<a class="description">Croissant saveur de France ...</a>
-					<a class="prix">1€</a>
-
-					<div class="add_sup">
-						<div class="command">
-							<a class="product_minus">-</a>
-							<div class="product_number">1</div>
-							<a class="product_plus">+</a>
-						</div>
-
-						<div >
-							<a class="button">ADD</a>
-						</div>
-					</div>
-
-				</div>
+			<div class="text_resume">
+				<table>
+					<tbody>
+						<tr>
+							<td class="nb_product">1×3 croissants 3,00 €</td>
+							<td class="prod_plus"><i class="fa fa-plus" aria-hidden="true"></td>
+							<td class="prod_minus"><i class="fa fa-minus" aria-hidden="true"></td>
+							<td class="prod_cancel"><i class="fa fa-times" aria-hidden="true"></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<a class="bottom">Total $10</a>
 
 			</div>
-
-			<div class="right-cart">
-				<a class="top"><i class="fa fa-shopping-cart" aria-hidden="true">&nbsp &nbsp</i>cart</a>
-				<div class="button button_order"><a href="cart.php" class="no-link">Validate your order</a></div>
-				<div class="text_pickup">
-					Pick-up : 11AM
-				</div>
-				<div class="text_resume">
-					<table>
-						<tbody>
-							<tr>
-								<td class="nb_product">1×3 croissants 3,00 €</td>
-								<td class="prod_plus"><i class="fa fa-plus" aria-hidden="true"></td>
-								<td class="prod_minus"><i class="fa fa-minus" aria-hidden="true"></td>
-								<td class="prod_cancel"><i class="fa fa-times" aria-hidden="true"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<a class="bottom">Total $10</a>
-
-				</div>
-
-		</div>
 
 	</div>
 
- 	<footer id="footer">
-        <div class="row">
-            <p>Bread express is a trademak brand. We want to make your life easier tasty too !</p>
-        </div>
-    </footer>
+</div>
 
-</body>
-
-</html>
+<?php
+    include 'assets/php/partials/footer.php';
+?>
 
 
 
